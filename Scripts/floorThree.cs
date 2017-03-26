7f63908cdd7e2a1e682b51e2efaffccf35794a1f
Jayant Tiwari <jayant.tiwari777@gmail.com>
2017-03-26 01:37:34 -0400

    working script

---
+++ Scripts/floorThree.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class floorThree : MonoBehaviour
+{
+    Vector3 originalPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab the original local position of the sphere when the app starts.
+        originalPosition = this.transform.localPosition;
+        this.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+
+    // Called by SpeechManager when the user says the "Reset world" command
+    void OnMorning()
+    {
+        // If the sphere has a Rigidbody component, remove it to disable physics.
+
+        this.GetComponent<Renderer>().enabled = false;
+        // Put the sphere back into its original local position.
+
+
+    }
+
+    void OnNoon()
+    {
+        this.GetComponent<Renderer>().enabled = false;
+        
+    }
+
+    void OnNight()
+    {
+        this.GetComponent<Renderer>().enabled = true;
+        this.transform.localPosition = originalPosition;
+    }
+
+    // Called by SpeechManager when the user says the "Drop sphere" command
+    void OnDrop()
+    {
+        // Just do the same logic as a Select gesture.
+        OnSelect();
+    }
+}
---
+++ Scripts/itemTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class itemTrigger : MonoBehaviour
+{
+    Vector3 originalPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab the original local position of the sphere when the app starts.
+
+        originalPosition = this.transform.localPosition;
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+
+
+            // add your stuff here
+        }
+    }
+}
